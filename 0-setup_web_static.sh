f7f0ff8e4f55899ae858917dd292a2a99c989172
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-04-06 12:05:31 +0300

    setting up web server for deployment

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-
 #This script is to setup the web servers for the deployment
 # of the web_static
 
@@ -19,12 +18,9 @@ ubuntu@89-web-01:~/$ curl localhost/hbnb_static/index.html
   </body>
 </html>'
 
-echo -e "Installing Nginx if not already installed"
-
 apt-get update
 apt-get install -y nginx
 
-echo -e "Setting up the directories\n"
 
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
@@ -36,9 +32,6 @@ ln -sf /data/web_static/releases/test /data/web_static/current
 
 chown -R ubuntu /data/
 chgrp -R ubuntu /data/
-
-
-echo -e "Updating the Server configuration\n"
 
 printf %s "server {
 	listen 80 default_server;
