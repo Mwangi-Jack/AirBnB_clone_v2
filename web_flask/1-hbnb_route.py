3ad6096423750caa63195c158d5425b3d5483339
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-04-20 14:09:57 +0300

    adding routes to a flask app

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+This script start a Flask web application
+The web application is expected to listen on 0.0.0.0 port 5000
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """
+    This function returns a string on route /
+    """
+    return "Hello HBNB!"
+
+@app.route('/hbnb')
+def hbnb():
+    """
+    On route /hbnb this function returns the strin 'HBNB'
+    """
+    return "HBNB"
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
