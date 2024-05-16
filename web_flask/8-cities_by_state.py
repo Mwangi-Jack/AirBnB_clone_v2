8ac668da0065769506e314f8715673c0dfc1f45e
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-05-16 14:54:01 +0300

    getting cities by states

--- web_flask/8-cities_by_state.py
+++ web_flask/8-cities_by_state.py
@@ -6,13 +6,14 @@ from flask import Flask
 from flask import render_template
 from models import *
 from models import storage
-
 app = Flask(__name__)
+
 
 @app.teardown_appcontext
 def teardown():
     """closes the database connection"""
     storage.close()
+
 
 @app.route('/cities_by_states', strict_slashes=False)
 def cities_by_states():
